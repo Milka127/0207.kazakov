23d7fdb3a724b039ed3026ff286ef4b26027dc0c
Ivan <75050033+Milka127@users.noreply.github.com>
2022-04-21 23:09:57 +0300

    Update botYandex.js
    version2.2

--- botYandex.js
+++ botYandex.js
@@ -5,17 +5,18 @@
 // @description  try to take over the world!
 // @author       BigShow
 // @match        https://yandex.ru/*
+// @match        https://auto.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 let keywords = [
-    "купля-продажа авто",
-    "каталог автомобилей",
-    "автомобили купить",
-    "б\у авто",
-    "купить BMW",
-    "купить Lada"
+        "купля-продажа авто",
+        "каталог автомобилей",
+        "автомобили купить",
+        "б\у авто",
+        "купить BMW",
+        "купить Lada"
 ];
 let num = keywords.length;
 let random = getRandom(0, num);
@@ -33,8 +34,8 @@ if(btn !== undefined) {
         if (i == keyword.length) {
             clearInterval(timerId);
         }
-        setTimeout(clickBtn, 10000);
-    },400);
+        setTimeout(clickBtn, 9500);
+    },350);
 } else if (location.hostname = "auto.ru") {
     setInterval(() => {
         if (getRandom(0, 101) >= 70) {
@@ -48,16 +49,16 @@ if(btn !== undefined) {
     for (let i = 0; i < links.length; i++) {
         if (links[i].href.indexOf("auto.ru") !== -1) {
             let link = links[i];
-            let nextPageYandex = false;
+            nextPageYandex = false;
             console.log("Нашел строку " + links[i]);
             // window.location.replace(link);
             setTimeout(() => {
-                linkNext();
+                link.click();
             }, getRandom(1500, 4000));
             break;
         }
     }
-    if (nextPage().innerText === "5") {
+    if (nextPage() === "5") {
         nextPageYandex = false;
         location.href = "https://www.yandex.ru/";
     }
@@ -69,14 +70,14 @@ if(btn !== undefined) {
 }
 
 function getRandom(min, max) {
-return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 function clickBtn() {
-  document.getElementsByClassName("button mini-suggest__button")[0].click();
-}
-function linkNext() {
-    document.getElementsByClassName("link_them_none")[0].click();
+    document.getElementsByClassName("button mini-suggest__button")[0].click();
 }
 function nextPage() {
-    document.getElementsByClassName("pager__item_current_yes")[0];
+    document.getElementsByClassName("pager__item_current_yes").innerText;
+}
+function linkNext() {
+    document.getElementsByClassName("pager__item_kind_next")[0].click();
 }
