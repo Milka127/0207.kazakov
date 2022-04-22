ac8a95d58467d3e6b04f0366fa5dfdc412a1b197
Ivan <75050033+Milka127@users.noreply.github.com>
2022-04-22 22:01:55 +0300

    Update botYandex.js
    version2.3

--- botYandex.js
+++ botYandex.js
@@ -17,67 +17,67 @@ let keywords = [
         "б\у авто",
         "купить BMW",
         "купить Lada"
-];
-let num = keywords.length;
-let random = getRandom(0, num);
-let keyword = keywords[random];
-let input = document.getElementsByName("text")[0];
-let btn = document.getElementsByClassName("button mini-suggest__button")[0];
-let links = document.links;
+    ];
+    let num = keywords.length;
+    let random = getRandom(0, num);
+    let keyword = keywords[random];
+    let input = document.getElementsByName("text")[0];
+    let btn = document.getElementsByClassName("button mini-suggest__button")[0];
+    let links = document.links;
 
-if(btn !== undefined) {
-    let i = 0;
-    console.log(btn);
-    let timerId = setInterval(function() {
-        input.value += keyword[i];
-        i++;
-        if (i == keyword.length) {
-            clearInterval(timerId);
-        }
-        setTimeout(clickBtn, 9500);
-    },350);
-} else if (location.hostname = "auto.ru") {
-    setInterval(() => {
-        if (getRandom(0, 101) >= 70) {
-            location.href = "https://www.yandex.ru/";
-        } else if (links[random].href.indexOf("auto.ru") !== -1) {
-            links[random].click();
-        }
-    }, getRandom(1500, 5000));
-} else {
-    let nextPageYandex = true;
-    for (let i = 0; i < links.length; i++) {
-        if (links[i].href.indexOf("auto.ru") !== -1) {
-            let link = links[i];
-            nextPageYandex = false;
-            console.log("Нашел строку " + links[i]);
-            // window.location.replace(link);
-            setTimeout(() => {
-                link.click();
-            }, getRandom(1500, 4000));
-            break;
+    if(btn !== undefined) {
+        let i = 0;
+        console.log(btn);
+        let timerId = setInterval(function() {
+            input.value += keyword[i];
+            i++;
+            if (i == keyword.length) {
+                clearInterval(timerId);
+            }
+            setTimeout(clickBtn, 9500);
+        },350);
+    } else if (location.hostname = "auto.ru") {
+        setInterval(() => {
+            if (getRandom(0, 101) >= 70) {
+                location.href = "https://www.yandex.ru/";
+            } else if (links[random].href.indexOf("auto.ru") !== -1) {
+                links[random].click();
+            }
+        }, getRandom(1500, 5000));
+    } else {
+        let nextPageYandex = true;
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].href.indexOf("https://www.yandex.ru/") !== -1) {
+                let link = links[i];
+                nextPageYandex = false;
+                console.log("Нашел строку " + links[i]);
+                // window.location.replace(link);
+                setTimeout(() => {
+                    link.click();
+                }, getRandom(1500, 4000));
+                break;
+            }
+            if (nextPage() === "5") {
+                nextPageYandex = false;
+                location.href = "https://www.yandex.ru/";
+            }
+            if (nextPageYandex) {
+                setTimeout(() => {
+                    linkNext();
+                }, getRandom(2000, 4000));
+            }
         }
     }
-    if (nextPage() === "5") {
-        nextPageYandex = false;
-        location.href = "https://www.yandex.ru/";
-    }
-    if (nextPageYandex) {
-        setTimeout(() => {
-            linkNext();
-        }, getRandom(2000, 4000));
-    }
-}
 
-function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-function clickBtn() {
-    document.getElementsByClassName("button mini-suggest__button")[0].click();
-}
-function nextPage() {
-    document.getElementsByClassName("pager__item_current_yes").innerText;
-}
-function linkNext() {
-    document.getElementsByClassName("pager__item_kind_next")[0].click();
-}
+    function getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    function clickBtn() {
+      document.getElementsByClassName("button mini-suggest__button")[0].click();
+    }
+    function nextPage() {
+        document.getElementsByClassName("pager__item_current_yes").innerText;
+    }
+    function linkNext() {
+        document.getElementsByClassName("pager__item_kind_next")[0].click();
+    }
